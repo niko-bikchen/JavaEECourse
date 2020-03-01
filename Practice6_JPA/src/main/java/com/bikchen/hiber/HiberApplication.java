17e80997a914d00ee9c01104757e12eeedcb7528
niko-bikchen <niko.bikcheb@gmail.com>
2020-03-01 23:23:38 +0200

    Finished practice 6

--- Practice6_JPA/src/main/java/com/bikchen/hiber/HiberApplication.java
+++ Practice6_JPA/src/main/java/com/bikchen/hiber/HiberApplication.java
@@ -4,12 +4,50 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import java.util.List;
+
 @SpringBootApplication
 public class HiberApplication {
 
     public static void main(String[] args) {
         ApplicationContext applicationContext = SpringApplication.run(HiberApplication.class, args);
         UserService userService = applicationContext.getBean(UserService.class);
+
+        UserEntity user1 = userService.createUser("John", "Doe");
+        UserEntity user2 = userService.createUser("Jane", "Doe");
+        UserEntity user3 = userService.createUser("Obi-Wan", "Kenobi");
+        UserEntity user4 = userService.createUser("Plo", "Koon");
+        UserEntity user5 = userService.createUser("Otto", "Kombi");
+
+        System.out.println("Saved user: " + user1);
+        System.out.println("Saved user: " + user2);
+        System.out.println("Saved user: " + user3);
+        System.out.println("Saved user: " + user4);
+        System.out.println("Saved user: " + user5);
+
+        System.out.println("Fetching all users");
+
+        List<UserEntity> allUsers = userService.getAllUsers();
+
+        for (UserEntity user : allUsers) {
+            System.out.println(user);
+        }
+
+        System.out.println("Fetching by last name");
+
+        List<UserEntity> allDoes = userService.getUsersByLastName("Doe");
+
+        for (UserEntity user : allDoes) {
+            System.out.println(user);
+        }
+
+        System.out.println("Fetching by substring");
+
+        List<UserEntity> haveBi = userService.getUsersBySubstring("bi");
+
+        for (UserEntity user : haveBi) {
+            System.out.println(user);
+        }
     }
 
 }
--- Practice6_JPA/src/main/java/com/bikchen/hiber/UserService.java
+++ Practice6_JPA/src/main/java/com/bikchen/hiber/UserService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -12,11 +13,37 @@ public class UserService {
     private final EntityManager entityManager;
 
     @Transactional
-    public UserEntity createUser(String first_name, String last_name, String email) {
+    public UserEntity createUser(String first_name, String last_name) {
         UserEntity user = new UserEntity();
         user.setFirstName(first_name);
         user.setLastName(last_name);
 
         return entityManager.merge(user);
+    }
+
+    @Transactional
+    public List<UserEntity> getAllUsers() {
+        return entityManager
+                .createQuery("select u from UserEntity u", UserEntity.class)
+                .getResultList();
+    }
+
+    @Transactional
+    public List<UserEntity> getUsersByLastName(String lName) {
+        return entityManager
+                .createQuery("select u from UserEntity u where u.lastName = :lName", UserEntity.class)
+                .setParameter("lName", lName)
+                .getResultList();
+    }
+
+    @Transactional
+    List<UserEntity> getUsersBySubstring(String substring) {
+        return entityManager
+                .createQuery("select u " +
+                        "from UserEntity u " +
+                        "where u.firstName like concat('%', :substring, '%')" +
+                        " or u.lastName like concat('%', :substring, '%')", UserEntity.class)
+                .setParameter("substring", substring)
+                .getResultList();
     }
 }
